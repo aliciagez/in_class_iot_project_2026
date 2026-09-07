ea8f8c6a1a323e23b99a1206be38320a23f5323a
Alicia <alicia.gezelius@gmail.com>
2026-09-07 13:41:41 +0200

    added more stuff

--- src_pipeline/comsumer.py
+++ src_pipeline/comsumer.py
@@ -1,12 +1,38 @@
 import paho.mqtt.client as mqtt
 import json
+from utils.connect_postgres import query_db
 
 
 def on_message(client, userdata, message):
     payload = message.payload.decode()
-    print(payload)
+    
+    print("connec to mqtt", payload, flush=True)
+
+    data=json.loads(payload)
+
+    temperature = float(data["temperature"])
+    humidity = float(data["humidity"])
+
+    query_db(
+        """
+        INSERT INTO sensor_readings 
+            (time, temperature, humidity) 
+        VALUES (NOW(), %s, %s)
+""",
+        (temperature, humidity),
+    )
+
+    print("save to database", temperature, humidity, flush=True)
 
 if __name__ == "__main__":
+    query_db("""
+        CREATE TABLE IF NOT EXISTS sensor_readings (
+            time TIMESTAMPTZ NOT NULL,
+            temperature DOUBLE PRECISION,
+            humidity DOUBLE PRECISION
+        )
+    """)
+
     client = mqtt.Client()
     client.connect("mosquitto", 1883)
     client.subscribe("home/pico/dht11")
--- src_pipeline/dockerfiles/consumer.dockerfile
+++ src_pipeline/dockerfiles/consumer.dockerfile
@@ -2,6 +2,7 @@ FROM python:3.13-slim
 
 WORKDIR /app
 COPY comsumer.py /app/
+COPY utils /app/utils
 COPY pyproject.toml /app/
 
 RUN pip install --no-cache-dir uv
